b48ef5bbbf49e3c05f5c2850b7d2d5720a9b57dd
Shiv Jain <70323345+shivjain123@users.noreply.github.com>
2021-06-19 16:02:24 +0530

    Add files via upload

---
+++ Project/medium_responses.py
@@ -0,0 +1,52 @@
+import csv
+import pandas as p
+import plotly.figure_factory as pff
+import plotly.graph_objects as go
+import statistics as s
+import random as r
+
+df = p.read_csv("D:/(4) WhiteHatJr. Codes/Third Module/Sampling/Project/Medium csv/medium_data.csv")
+
+responses = df["responses"].tolist()
+
+figure = pff.create_distplot([responses], ["Results"], show_hist=False)
+
+#figure.show()
+
+population_mean = s.mean(responses)
+population_sd = s.stdev(responses)
+
+print()
+print("The Old Mean is {mean}" .format(mean=population_mean))
+print()
+print("The Old Standard Deviaton {sd}" .format(sd=population_sd))
+print()
+
+def random_set_of_mean(counter):
+    data_set = list()
+    for d in range(counter):
+        random_i = r.randint(0, len(responses) - 1)
+        data_set.append(responses[random_i])
+    mean_of_data_set = s.mean(data_set)
+    return mean_of_data_set
+
+def draw_figure(data_set_n):
+    figure = pff.create_distplot([data_set_n], ["Results"], show_hist=False)
+    figure.add_trace(go.Scatter(x=[population_mean, population_mean], y=[0, 0.005], mode="lines", name="Mean"))
+    figure.show()
+
+def setup():
+    list_of_means = list()
+    for i in range(1000):
+        set_of_means = random_set_of_mean(100)
+        list_of_means.append(set_of_means)
+    draw_figure(list_of_means)
+    mean_of_everything = s.mean(list_of_means)
+    final_sd = s.stdev(list_of_means)
+    print("Mean of Sampling Distribution {moe}".format(moe=mean_of_everything))
+    print()
+    print(
+        "Standard Deviation of the Sampling Distribution {f}".format(f=final_sd))
+    print()
+
+setup()
